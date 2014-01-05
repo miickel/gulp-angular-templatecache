fe8c4e6957383bacd00ee4077eea99790d052f07
Mickel <andersson.mickel@gmail.com>
2014-01-06 00:32:58 +0100

    init

---
+++ index.js
@@ -0,0 +1,63 @@
+var es = require('event-stream');
+var path = require('path');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
+var htmlJsStr = require('js-string-escape');
+
+module.exports = function(filename, options) {
+	if (!filename) {
+		throw new PluginError('gulp-concat', 'Missing filename option for gulp-angular-templatecache');
+	}
+	if (!options) {
+		options = {};
+	}
+
+	var buffer = [];
+	var firstFile;
+
+	function bufferFiles(file) {
+		if (file.isNull()) {
+			return;
+		}
+		if (file.isStream()) {
+			return this.emit('error', new PluginError('gulp-angular-templatecache', 'Streaming not supported'));
+		}
+		if (!firstFile) {
+			firstFile = file;
+		}
+
+		buffer.push(file);
+	}
+
+	function endStream() {
+		if (buffer.length === 0) {
+			return this.emit('end');
+		}
+
+		var result = 'angular.module("templates", []).run([$templateCache, function($templateCache) {';
+
+		result += buffer.map(function(file) {
+			var url = file.path.replace(file.base, '');
+			var template = '$templateCache.put("' + url + '","';
+			template += htmlJsStr(file.contents);
+			template += '");';
+			return template;
+		}).join('');
+
+		result += '}]);';
+
+		var resultPath = path.join(firstFile.base, filename);
+
+		var templatesFile = new gutil.File({
+			cwd: buffer[0].cwd,
+			base: buffer[0].base,
+			path: resultPath,
+			contents: new Buffer(result)
+		});
+
+		this.emit('data', templatesFile);
+		this.emit('end');
+	}
+
+	return es.through(bufferFiles, endStream);
+};
---
+++ test.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+var gutil = require('gulp-util');
+var templateCache = require('./index');
+
+it('should build valid $templateCache from two html-files', function(cb) {
+	var stream = templateCache('templates.js');
+
+	stream.on('data', function(file) {
+		assert.equal(file.path, '~/dev/projects/gulp-angular-templatecache/test/templates.js');
+		assert.equal(file.relative, 'templates.js');
+		assert.equal(file.contents.toString('utf8'), 'angular.module("templates", []).run([$templateCache, function($templateCache) {$templateCache.put("/template-a.html","<h1 id=\\"template-a\\">I\\\'m template A!</h1>");$templateCache.put("/template-b.html","<h1 id=\\"template-b\\">I\\\'m template B!</h1>");}]);');
+		cb();
+	});
+
+	stream.write(new gutil.File({
+		base: '~/dev/projects/gulp-angular-templatecache/test',
+		path: '~/dev/projects/gulp-angular-templatecache/test/template-a.html',
+		contents: new Buffer('<h1 id="template-a">I\'m template A!</h1>')
+	}));
+
+	stream.write(new gutil.File({
+		base: '~/dev/projects/gulp-angular-templatecache/test',
+		path: '~/dev/projects/gulp-angular-templatecache/test/template-b.html',
+		contents: new Buffer('<h1 id="template-b">I\'m template B!</h1>')
+	}));
+
+	stream.end();
+});

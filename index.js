8bfd061cd210222d03a9c5bcbe9de9a3a513cf31
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2014-01-18 22:11:06 +0100

    Simplify plugin by using existing plugins

--- index.js
+++ index.js
@@ -1,61 +1,41 @@
 var es = require('event-stream');
 var path = require('path');
 var gutil = require('gulp-util');
+var concat = require('gulp-concat');
+var header = require('gulp-header');
+var footer = require('gulp-footer');
 var PluginError = gutil.PluginError;
 var htmlJsStr = require('js-string-escape');
+
+function templateCache(root) {
+	return es.map(function(file, cb) {
+		var template = '$templateCache.put("<%= url %>","<%= contents %>");';
+
+		file.contents = new Buffer(gutil.template(template, {
+			url: path.join(root, file.path.replace(file.base, '')),
+			contents: htmlJsStr(file.contents),
+			file: file
+		}));
+
+		cb(null, file);
+	});
+}
 
 module.exports = function(filename, options) {
 	if (!filename) {
 		throw new PluginError('gulp-angular-templatecache', 'Missing filename option for gulp-angular-templatecache');
 	}
 	options = options || {};
-	options.root = options.root || '';
-	options.module = options.module || 'templates';
 
-	var buffer = [];
+	var templateHeader = "angular.module('<%= module %>').run(['$templateCache', function($templateCache) {\n";
+	var templateFooter = "}]);\n"
 
-	function bufferFiles(file) {
-		if (file.isNull()) {
-			return;
-		}
-		if (file.isStream()) {
-			return this.emit('error', new PluginError('gulp-angular-templatecache', 'Streaming not supported'));
-		}
-
-		buffer.push(file);
-	}
-
-	function templateCache(file) {
-		var template = '$templateCache.put("<%= url %>","<%= contents %>");';
-
-		return gutil.template(template, {
-			url: path.join(options.root, file.path.replace(file.base, '')),
-			contents: htmlJsStr(file.contents),
-			file: file
-		});
-	}
-
-	function endStream() {
-		if (buffer.length === 0) {
-			return this.emit('end');
-		}
-
-		var result = 'angular.module("' + options.module + '", []).run(["$templateCache", function($templateCache) {';
-		result += buffer.map(templateCache).join('');
-		result += '}]);';
-
-		var resultPath = path.join(buffer[0].base, filename);
-
-		var templatesFile = new gutil.File({
-			cwd: buffer[0].cwd,
-			base: buffer[0].base,
-			path: resultPath,
-			contents: new Buffer(result)
-		});
-
-		this.emit('data', templatesFile);
-		this.emit('end');
-	}
-
-	return es.through(bufferFiles, endStream);
+	return es.pipeline(
+		templateCache(options.root || ''),
+		concat(filename),
+		header(templateHeader, {
+			module: options.module || 'templates'
+		}),
+		footer(templateFooter)
+	)
 };

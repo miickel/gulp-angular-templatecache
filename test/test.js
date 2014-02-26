f5db7029a524fb83af7ccfde888a9f6b77911c9e
Kevin King <kevinking@Kevins-MacBook-Pro.local>
2014-02-25 19:13:28 -0800

    feat: base path is relative to file's current path

--- test/test.js
+++ test/test.js
@@ -136,18 +136,18 @@ it('can override file base path in options', function(cb) {
 	var stream = templateCache({
 		standalone: true,
 		root: '/views',
-		base: '~/dev/projects/gulp-angular-templatecache'
+		base: 'test'
 	});
 
 	stream.on('data', function(file) {
-		assert.equal(file.path, '~/dev/projects/gulp-angular-templatecache/test/templates.js');
+		assert.equal(file.path, '~/dev/projects/gulp-angular-templatecache/templates.js');
 		assert.equal(file.relative, 'templates.js');
-		assert.equal(file.contents.toString('utf8'), 'angular.module("templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("/views/test/template-a.html","<h1 id=\\"template-a\\">I\\\'m template A!</h1>");}]);');
+		assert.equal(file.contents.toString('utf8'), 'angular.module("templates", []).run(["$templateCache", function($templateCache) {$templateCache.put("/views/template-a.html","<h1 id=\\"template-a\\">I\\\'m template A!</h1>");}]);');
 		cb();
 	});
 
 	stream.write(new gutil.File({
-		base: '~/dev/projects/gulp-angular-templatecache/test',
+		base: '~/dev/projects/gulp-angular-templatecache/',
 		path: '~/dev/projects/gulp-angular-templatecache/test/template-a.html',
 		contents: new Buffer('<h1 id="template-a">I\'m template A!</h1>')
 	}));

11d3dd505cf8a0f1ed476b91508ee96051cce840
Mickel Andersson <andersson.mickel@gmail.com>
2014-09-21 13:52:49 +0200

    refactor: extract anonymous functions, add comments, style changes

--- index.js
+++ index.js
@@ -6,16 +6,31 @@ var header = require('gulp-header');
 var footer = require('gulp-footer');
 var htmlJsStr = require('js-string-escape');
 
-function templateCache(root, base) {
-  if (typeof base !== 'function' && base && base.substr(-1) !== path.sep) {
-    base += '/';
-  }
+/**
+ * "constants"
+ */
 
-  return es.map(function(file, callback) {
+var TEMPLATE_HEADER = 'angular.module("<%= module %>"<%= standalone %>).run(["$templateCache", function($templateCache) {';
+var TEMPLATE_FOOTER = '}]);';
+var DEFAULT_FILENAME = 'templates.js';
+var DEFAULT_MODULE = 'templates';
+
+
+/**
+ * Add files to templateCache.
+ */
+
+function templateCacheFiles(root, base) {
+
+  return function templateCacheFile(file, callback) {
     var template = '$templateCache.put("<%= url %>","<%= contents %>");';
     var url;
 
     file.path = path.normalize(file.path);
+
+    /**
+     * Rewrite url
+     */
 
     if (typeof base === 'function') {
       url = path.join(root, base(file));
@@ -23,9 +38,17 @@ function templateCache(root, base) {
       url = path.join(root, file.path.replace(base || file.base, ''));
     }
 
+    /**
+     * Normalize url (win only)
+     */
+
     if (process.platform === 'win32') {
       url = url.replace(/\\/g, '/');
     }
+
+    /**
+     * Create buffer
+     */
 
     file.contents = new Buffer(gutil.template(template, {
       url: url,
@@ -34,27 +57,74 @@ function templateCache(root, base) {
     }));
 
     callback(null, file);
-  });
+
+  };
+
 }
 
-module.exports = function(filename, options) {
+
+/**
+ * templateCache a stream of files.
+ */
+
+function templateCacheStream(root, base) {
+
+  /**
+   * Set relative base
+   */
+
+  if (typeof base !== 'function' && base && base.substr(-1) !== path.sep) {
+    base += '/';
+  }
+
+  /**
+   * templateCache files
+   */
+
+  return es.map(templateCacheFiles(root, base));
+
+}
+
+
+/**
+ * Concatenates and registers AngularJS templates in the $templateCache.
+ *
+ * @param {string} [filename='templates.js']
+ * @param {object} [options]
+ */
+
+function templateCache(filename, options) {
+
+  /**
+   * Prepare options
+   */
+
   if (typeof filename === 'string') {
     options = options || {};
   } else {
     options = filename || {};
-    filename = options.filename || 'templates.js';
+    filename = options.filename || DEFAULT_FILENAME;
   }
 
-  var templateHeader = 'angular.module("<%= module %>"<%= standalone %>).run(["$templateCache", function($templateCache) {';
-  var templateFooter = '}]);';
+  /**
+   * Build templateCache
+   */
 
   return es.pipeline(
-    templateCache(options.root || '', options.base),
+    templateCacheStream(options.root || '', options.base),
     concat(filename),
-    header(templateHeader, {
-      module: options.module || 'templates',
+    header(TEMPLATE_HEADER, {
+      module: options.module || DEFAULT_MODULE,
       standalone: options.standalone ? ', []' : ''
     }),
-    footer(templateFooter)
+    footer(TEMPLATE_FOOTER)
   );
-};
+
+}
+
+
+/**
+ * Expose templateCache
+ */
+
+module.exports = templateCache;

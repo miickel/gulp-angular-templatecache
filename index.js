ede77d6970688d05cb36613b9e9047063dbb0b43
Mark Fayngersh <mark.fayngersh@gmail.com>
2015-03-24 20:59:11 -0400

    Avoid processing files multiple times.
    See: https://github.com/miickel/gulp-angular-templatecache/pull/36

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ var MODULE_TEMPLATES = {
 function templateCacheFiles(root, base) {
 
   return function templateCacheFile(file, callback) {
+    if (file.processedByTemplateCache) {
+      return callback(null, file);
+    }
+
     var template = '$templateCache.put("<%= url %>","<%= contents %>");';
     var url;
 
@@ -66,6 +70,8 @@ function templateCacheFiles(root, base) {
       contents: htmlJsStr(file.contents),
       file: file
     }));
+
+    file.processedByTemplateCache = true;
 
     callback(null, file);
 

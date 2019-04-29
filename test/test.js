26ce3f7ce9fadb510bad1c9afbd910541e29d8a3
Tereza Tomcova <the-ress@users.noreply.github.com>
2019-04-29 13:26:20 +0200

    fix: don't try to process directories (#145) (#171)

--- test/test.js
+++ test/test.js
@@ -53,6 +53,34 @@ describe('gulp-angular-templatecache', function () {
     stream.end();
   });
 
+  it('should ignore directories', function (cb) {
+    var stream = templateCache();
+
+    stream.on('data', function (file) {
+      assert.equal(path.normalize(file.path), path.normalize(__dirname + '/templates.js'));
+      assert.equal(file.relative, 'templates.js');
+      assert.equal(file.contents.toString('utf8'), 'angular.module(\'templates\').run([\'$templateCache\', function($templateCache) {$templateCache.put(\'/directory/template-a.html\',\'<h1 id="template-a">I\\\'m template A!</h1>\');}]);');
+      cb();
+    });
+
+    stream.write(new Vinyl({
+      base: __dirname,
+      path: __dirname + '/directory',
+      contents: null,
+      stat: {
+        isDirectory: () => true
+      }
+    }));
+
+    stream.write(new Vinyl({
+      base: __dirname,
+      path: __dirname + '/directory/template-a.html',
+      contents: new Buffer('<h1 id="template-a">I\'m template A!</h1>')
+    }));
+
+    stream.end();
+  });
+
 
   describe('options.root', function () {
 
